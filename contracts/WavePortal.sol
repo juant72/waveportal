840e34aa6346f9d260ee4444147f5be709b22940
Juan Tapia <juant72@gmail.com>
2022-07-18 09:25:36 -0300

    First

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal{
+    constructor (){
+        console.log("Yo yo, I am a contractor and I am smart");
+    }
+
+}
+
